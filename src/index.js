ff85c926bf476426c7a2bede396fb6b7fb26e14f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-29 18:52:18 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -62,7 +62,9 @@ export class App extends React.Component {
   render() {
     return (
       <div id="app">
-
+      <OlderCoaster />
+      <InFrontOfYou />
+      <ButcherShop />
       </div>
     )
   }
