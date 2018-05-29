431d5e4110420a1bff29fe5bc87ea5680f3f8c73
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-29 18:46:23 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -30,6 +30,14 @@ export class OlderCoaster extends React.Component {
 
 export class InFrontOfYou extends React.Component {
   // your code here
+  render() {
+    return (
+      <div>
+      <p>You shouldnt look too far.</p>
+      <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
 
 export class ButcherShop extends React.Component {
